6b01b3effc47f18050f617026d787a3e4cc4af69
YuriiKyryliuk <yuriik@anybiz.io>
2024-01-16 21:35:51 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <span>{{ data }}</span>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+const data = ref({})
+
+async function getDataFromAxios() {
+  try {
+    const response = await axios.get('//jsonplaceholder.typicode.com/users')
+    data.value = response.data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+}
+
+onMounted(async () => {
+  await getDataFromAxios()
+  console.log('data', data)
+})
+</script>
