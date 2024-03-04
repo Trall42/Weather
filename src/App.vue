2045b80c081a18fc712c60fd8d6fe8156f56e8b2
YuriiKyryliuk <yuriik@anybiz.io>
2024-03-04 21:38:47 +0200

    Remove comment files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" class="app">
+    <RootBlock />
+  </div>
+</template>
+
+<script setup>
+import RootBlock from './components/RootBlock.vue'
+</script>
+<style lang="scss">
+html {
+  background: $white !important;
+}
+.app {
+  width: 100%;
+  height: 100vh;
+  background-image: url('/src/assets/img/cities/city-1.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding: 24px;
+}
+@media(min-width: 768px) {
+  .app {
+    background-image: url('/src/assets/img/weather_wallpaper/summer.webp');
+  }
+}
+</style>
